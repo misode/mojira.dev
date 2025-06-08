404867ab784c92f71e619a6577b68ccef946092d
Misode <misoloo64@gmail.com>
2025-06-08 18:23:01 +0200

    Modify migrations to match production

--- migrations/008_fix_null_fields.sql
+++
@@ -1,4 +0,0 @@
--- Set null comments to empty strings
-UPDATE issue SET creator_name = '' WHERE creator_name IS NULL;
-UPDATE issue SET creator_avatar = '' WHERE creator_avatar IS NULL;
-UPDATE comment SET legacy_id = '' WHERE legacy_id IS NULL;
